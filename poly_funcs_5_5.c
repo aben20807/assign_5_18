74fa4d997f9dbf66c1744f6d2de7607a013259af
aben20807 <aben20807@gmail.com>
2018-11-15 17:46:36 +0800

    Find optimal code, need to be organized

--- poly_funcs_5_5.c
+++ poly_funcs_5_5.c
@@ -1,4 +1,4 @@
-double poly_5_05(double a[], double x, long degree)
+double poly_5_05__(double a[], double x, long degree)
 {
     long i;
     double result = a[0];
@@ -17,7 +17,7 @@ double poly_5_05_rev_2(double a[], double x, long degree)
     double result_1 = 0;
     double xpwr = x;
     double x_pow_2 = x * x;
-    for (i = 1; i < degree - 1; i += 2) {
+    for (i = 1; i <= degree - 1; i += 2) {
         result_0 += a[i] * xpwr;
         result_1 += a[i + 1] * xpwr * x;
         xpwr = xpwr * x_pow_2;
@@ -30,7 +30,7 @@ double poly_5_05_rev_2(double a[], double x, long degree)
     return result_0 + result_1;
 }
 
-double poly_5_05_rev_3(double a[], double x, long degree)
+double poly_5_05_3(double a[], double x, long degree)
 {
     long i;
     double result_0 = a[0];
@@ -39,10 +39,11 @@ double poly_5_05_rev_3(double a[], double x, long degree)
     double xpwr = x;
     double x_pow_2 = x * x;
     double x_pow_3 = x * x_pow_2;
-    for (i = 1; i < degree - 1; i += 3) {
-        result_0 += a[i] * xpwr;
-        result_1 += a[i + 1] * xpwr * x;
-        result_2 += a[i + 2] * xpwr * x_pow_2;
+    for (i = 1; i <= degree - 2; i += 3) {
+        // result_0 += a[i] * xpwr;
+        // result_1 += a[i + 1] * xpwr * x;
+        // result_2 += a[i + 2] * xpwr * x_pow_2;
+        result_0 += (a[i] + a[i + 1] * x + a[i + 2] * x_pow_2) * xpwr;
         xpwr = xpwr * x_pow_3;
     }
 
@@ -53,7 +54,7 @@ double poly_5_05_rev_3(double a[], double x, long degree)
     return result_0 + result_1 + result_2;
 }
 
-double poly_5_05_rev(double a[], double x, long degree)
+double poly_5_05_rev_4(double a[], double x, long degree)
 {
     long i;
     double result_0 = a[0];
@@ -64,7 +65,7 @@ double poly_5_05_rev(double a[], double x, long degree)
     double x_pow_2 = x * x;
     double x_pow_3 = x * x_pow_2;
     double x_pow_4 = x * x_pow_3;
-    for (i = 1; i < degree - 2; i += 4) {
+    for (i = 1; i <= degree - 3; i += 4) {
         result_0 += a[i] * xpwr;
         result_1 += a[i + 1] * xpwr * x;
         result_2 += a[i + 2] * xpwr * x_pow_2;
@@ -77,4 +78,174 @@ double poly_5_05_rev(double a[], double x, long degree)
         xpwr = xpwr * x;
     }
     return result_0 + result_1 + result_2 + result_3;
+}
+
+double poly_5_05_rev_5(double a[], double x, long degree)
+{
+    long i;
+    double result_0 = a[0];
+    double result_1 = 0;
+    double result_2 = 0;
+    double result_3 = 0;
+    double result_4 = 0;
+    double xpwr = x;
+    double x_pow_2 = x * x;
+    double x_pow_3 = x * x_pow_2;
+    double x_pow_4 = x * x_pow_3;
+    double x_pow_5 = x * x_pow_4;
+    for (i = 1; i <= degree - 4; i += 5) {
+        result_0 += a[i] * xpwr;
+        result_1 += a[i + 1] * xpwr * x;
+        result_2 += a[i + 2] * xpwr * x_pow_2;
+        result_3 += a[i + 3] * xpwr * x_pow_3;
+        result_4 += a[i + 4] * xpwr * x_pow_4;
+        xpwr = xpwr * x_pow_5;
+    }
+
+    for (; i <= degree; i++) {
+        result_0 += a[i] * xpwr;
+        xpwr = xpwr * x;
+    }
+    return result_0 + result_1 + result_2 + result_3 + result_4;
+}
+
+double poly_5_05(double a[], double x, long degree)
+{
+    long i;
+    double result_0 = a[0];
+    double result_1 = 0;
+    double result_2 = 0;
+    double result_3 = 0;
+    double result_4 = 0;
+    double result_5 = 0;
+    double xpwr = x;
+    double x_pow_2 = x * x;
+    double x_pow_3 = x * x_pow_2;
+    double x_pow_4 = x * x_pow_3;
+    double x_pow_5 = x * x_pow_4;
+    double x_pow_6 = x * x_pow_5;
+    for (i = 1; i <= degree - 5; i += 6) {
+        result_0 += (a[i] + a[i + 1] * x + a[i + 2] * x_pow_2) * xpwr;
+        result_3 +=
+            (a[i + 3] * x_pow_3 + a[i + 4] * x_pow_4 + a[i + 5] * x_pow_5) *
+            xpwr;
+        xpwr = xpwr * x_pow_6;
+    }
+
+    for (; i <= degree; i++) {
+        result_0 += a[i] * xpwr;
+        xpwr = xpwr * x;
+    }
+    return result_0 + result_1 + result_2 + result_3 + result_4 + result_5;
+}
+
+double poly_5_05_rev(double a[], double x, long degree)
+{
+    long i;
+    double result_0 = a[0];
+    double result_1 = 0;
+    double result_2 = 0;
+    double result_3 = 0;
+    double result_4 = 0;
+    double result_5 = 0;
+    double xpwr = x;
+    double x_pow_2 = x * x;
+    double x_pow_3 = x * x_pow_2;
+    double x_pow_4 = x * x_pow_3;
+    double x_pow_5 = x * x_pow_4;
+    double x_pow_6 = x * x_pow_5;
+    double x_pow_7 = x * x_pow_6;
+    double x_pow_8 = x * x_pow_7;
+    double x_pow_9 = x * x_pow_8;
+    for (i = 1; i <= degree - 8; i += 9) {
+        result_0 += (a[i] + a[i + 1] * x + a[i + 2] * x_pow_2) * xpwr;
+        result_3 +=
+            (a[i + 3] * x_pow_3 + a[i + 4] * x_pow_4 + a[i + 5] * x_pow_5) *
+            xpwr;
+        result_4 +=
+            (a[i + 6] * x_pow_6 + a[i + 7] * x_pow_7 + a[i + 8] * x_pow_8) *
+            xpwr;
+        xpwr = xpwr * x_pow_9;
+    }
+
+    for (; i <= degree; i++) {
+        result_0 += a[i] * xpwr;
+        xpwr = xpwr * x;
+    }
+    return result_0 + result_1 + result_2 + result_3 + result_4 + result_5;
+}
+
+double poly_5_05_rev_7(double a[], double x, long degree)
+{
+    long i;
+    double result_0 = a[0];
+    double result_1 = 0;
+    double result_2 = 0;
+    double result_3 = 0;
+    double result_4 = 0;
+    double result_5 = 0;
+    double result_6 = 0;
+    double xpwr = x;
+    double x_pow_2 = x * x;
+    double x_pow_3 = x * x_pow_2;
+    double x_pow_4 = x * x_pow_3;
+    double x_pow_5 = x * x_pow_4;
+    double x_pow_6 = x * x_pow_5;
+    double x_pow_7 = x * x_pow_6;
+    for (i = 1; i <= degree - 6; i += 7) {
+        result_0 += a[i] * xpwr;
+        result_1 += a[i + 1] * xpwr * x;
+        result_2 += a[i + 2] * xpwr * x_pow_2;
+        result_3 += a[i + 3] * xpwr * x_pow_3;
+        result_4 += a[i + 4] * xpwr * x_pow_4;
+        result_5 += a[i + 5] * xpwr * x_pow_5;
+        result_6 += a[i + 6] * xpwr * x_pow_6;
+        xpwr = xpwr * x_pow_7;
+    }
+
+    for (; i <= degree; i++) {
+        result_0 += a[i] * xpwr;
+        xpwr = xpwr * x;
+    }
+    return result_0 + result_1 + result_2 + result_3 + result_4 + result_5 +
+           result_6;
+}
+
+double poly_5_05_rev_(double a[], double x, long degree)
+{
+    long i;
+    double result_0 = a[0];
+    double result_1 = 0;
+    double result_2 = 0;
+    double result_3 = 0;
+    double result_4 = 0;
+    double result_5 = 0;
+    double result_6 = 0;
+    double result_7 = 0;
+    double xpwr = x;
+    double x_pow_2 = x * x;
+    double x_pow_3 = x * x_pow_2;
+    double x_pow_4 = x * x_pow_3;
+    double x_pow_5 = x * x_pow_4;
+    double x_pow_6 = x * x_pow_5;
+    double x_pow_7 = x * x_pow_6;
+    double x_pow_8 = x * x_pow_7;
+    for (i = 1; i <= degree - 7; i += 8) {
+        result_0 += a[i] * xpwr;
+        result_1 += a[i + 1] * xpwr * x;
+        result_2 += a[i + 2] * xpwr * x_pow_2;
+        result_3 += a[i + 3] * xpwr * x_pow_3;
+        result_4 += a[i + 4] * xpwr * x_pow_4;
+        result_5 += a[i + 5] * xpwr * x_pow_5;
+        result_6 += a[i + 6] * xpwr * x_pow_6;
+        result_7 += a[i + 7] * xpwr * x_pow_7;
+        xpwr = xpwr * x_pow_8;
+    }
+
+    for (; i <= degree; i++) {
+        result_0 += a[i] * xpwr;
+        xpwr = xpwr * x;
+    }
+    return result_0 + result_1 + result_2 + result_3 + result_4 + result_5 +
+           result_6 + result_7;
 }
--- poly_funcs_5_5.h
+++ poly_funcs_5_5.h
@@ -2,6 +2,9 @@
 #define POLY_FUNC_5_5H
 
 double poly_5_05(double a[], double x, long degree);
+double poly_5_05_rev_2(double a[], double x, long degree);
+double poly_5_05_rev_3(double a[], double x, long degree);
+double poly_5_05_rev_4(double a[], double x, long degree);
 double poly_5_05_rev(double a[], double x, long degree);
 
 #endif /* POLY_FUNC_5_5H */
